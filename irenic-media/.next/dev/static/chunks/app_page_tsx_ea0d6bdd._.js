319a5fd597104d91bcfec9ba3fe9d2fd80ff7fde
Raj Shah <raj.shah@kaizeninfotech.com>
2026-02-04 00:15:06 +0530

    Remove node_modules and .next, add .gitignore

--- irenic-media/.next/dev/static/chunks/app_page_tsx_ea0d6bdd._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_348ea4c4._.js",
-  "static/chunks/node_modules_330bd4ca._.js"
-],
-    source: "dynamic"
-});
